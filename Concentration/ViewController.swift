142b708693de4203769ae8136553b3e595ceb68c
Nicolai <nicovatne@gmail.com>
2019-08-23 14:53:43 +0200

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Nicolai Vatne on 23/08/2019.
+//  Copyright © 2019 Nicolai Vatne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
